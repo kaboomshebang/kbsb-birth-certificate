1cb1fd4c554a6970c482f46e07fe8e09573af80b
Fred <fred@fastmail.nl>
2022-01-10 05:21:55 +0100

    Hello world contract

---
+++ contracts/examples/00_HelloKbsb.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+/**
+* You could use this very simple example contract to test a deploy to the Mainnet
+*/
+
+contract HelloKbsb {
+
+    string private shebang = "#!/kaboom/shebang";
+
+    function printShebang() public view returns (string memory) {
+        return shebang;
+    }
+}
